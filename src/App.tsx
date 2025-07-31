311055634257c09bbf78150e62e9ea2986cabf54
Alquimara <alquimara.alves@w4bempreendimentos.com.br>
2025-07-31 19:27:43 -0300

    realizado a refatoração do codigo e adicionado a a feature de nao poder interagir com os cards, quando estiverem concluidos

--- src/App.tsx
+++ src/App.tsx
@@ -50,8 +50,7 @@ function App() {
     setEncontrosCard,
     setCardRealizados,
     setCardRevelados,
-    toggleAmei,
-    cardsAmados,
+
     setFaseAtual,
     faseAtual,
     setNavegandoFaseConcluida,
@@ -59,6 +58,7 @@ function App() {
 
   } = useEncontros(Encontros,faseSelecionada);
   const [jogoConcluido,setJogoConcluido] = useState(false)
+
 
 
 
@@ -92,10 +92,9 @@ function App() {
 
 
   useEffect(() => {
-    // if (faseSelecionada === null) return;
-    // const fase = faseSelecionada ?? faseAtual;
+  
     if (faseSelecionada === null) {
-      // Não checa nada! Está na tela de fases!
+
       return;
     }
   
@@ -120,7 +119,7 @@ function App() {
 
 
   const handleMudancaFase = () => {
-    // const faseAtual = Number(localStorage.getItem('faseAtual') || '0');
+
   
     if (modalInfo?.tipo === 'fase') {
       const novaFase = faseAtual + 1;
@@ -133,13 +132,7 @@ function App() {
   
   
       localStorage.setItem('faseAtual', novaFase.toString());
-      // localStorage.removeItem('cardRealizados');
-      // localStorage.removeItem('cardRevelados');
-  
-      // const categoriasAntigas = Object.keys(todasAsFases[faseAtual] || {});
-      // categoriasAntigas.forEach((categoria) => {
-      //   localStorage.removeItem(concluida-${categoria}-fase-${faseAtual});
-      // });
+   
       setFaseAtual(novaFase);
   
       setFaseSelecionada(novaFase);
@@ -165,83 +158,14 @@ function App() {
   }
 
   return (
-    // <>
-    // <Toaster richColors /><div className="p-6 max-w-6xl mx-auto">
-    //   {!categoriaSelecionada ? (
-    //     <>
-    //       <HeaderEncontro title="💘 Encontros Secretos" />
-    //       <p className="text-center text-neutral-700 mb-10 text-lg max-w-xl mx-auto">
-    //         Descubra encontros únicos e surpreendentes. Clique em uma categoria para começar.
-    //       </p>
 
-    //       <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
-    //         <Fases/>
-    //         {categorias.map((categoria) => (
-    //           <CategoriaCard
-    //             key={categoria}
-    //             tipo='categoria'
-    //             nome={categoria}
-    //             total={encontrosCard[categoria]?.length || 0}
-    //             feitos={getQtdRealizados(categoria)}
-    //             onClick={() => setCategoriaSelecionada(categoria)}
-    //             icone={iconePorCategoria[categoria] || <span>❓</span>} 
-    //             isCompleto = {getQtdRealizados(categoria) === encontrosCard[categoria]?.length}
-    //             />
-    //         ))}
-    //       </div>
-    //     </>
-    //   ) : (
-    //     <>
-    //       <div className="relative flex items-center mb-8 justify-center">
-    //         <div className="absolute left-0">
-    //           <ButtonVoltar onClick={() => setCategoriaSelecionada(null)} />
-    //         </div>
-
-    //         <div className="mx-auto">
-    //           <HeaderEncontro
-    //             title={<span>
-    //               {iconePorCategoria[categoriaSelecionada] || '❓'} {categoriaSelecionada}
-    //             </span>} />
-    //         </div>
-    //       </div>
-
-    //       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 mt-15">
-    //         {encontrosCard[categoriaSelecionada]?.map((encontro, index) => {
-    //           const key = `${categoriaSelecionada}-${encontro.id}`;
-    //           const isFeito = cardRealizados[key];
-
-    //           return (
-    //             <EncontroCard
-    //             categoria={categoriaSelecionada} 
-    //               key={index}
-    //               encontro={encontro}
-    //               revelado={cardRevelados[key]}
-    //               realizado={isFeito}
-    //               onRevelar={() => revelarCard(categoriaSelecionada, encontro.id)}
-    //               onToggleRealizado={() => toggleRealizado(categoriaSelecionada, encontro.id)}
-    //               toggleAmei={() => toggleAmei(categoriaSelecionada, encontro.id)} cardsAmados={cardsAmados} />
-    //           );
-    //         })}
-    //       </div>
-    //     </>
-    //   )}
-
-    //   {modalInfo && (
-    //     <ModalParabens
-    //       titulo={modalInfo.titulo}
-    //       mensagem={modalInfo.mensagem}
-    //       onClose={handleMudancaFase} />
-    //   )}
-
-
-    // </div>
     
   
      
      
     
     
-    // </>
+
     <>
     <Toaster richColors />
     <div className="p-6 max-w-6xl mx-auto">
@@ -331,8 +255,7 @@ function App() {
                   onToggleRealizado={() =>
                     toggleRealizado(categoriaSelecionada, encontro.id)
                   }
-                  toggleAmei={() => toggleAmei(categoriaSelecionada, encontro.id)}
-                  cardsAmados={cardsAmados}
+                  faseConcluida={navegandoFaseConcluida}
                 />
               );
             })}
@@ -366,228 +289,6 @@ export default App;
 
 
 
-// import './global.css';
-// import { HeaderEncontro } from './components/Header';
-// import { ButtonVoltar } from './components/ButtonVoltar';
-// import { EncontroCard } from './components/EncontroCard';
-// import { ModalParabens } from './components/modalSucess';
-// import FinalScreen from './components/FinalScreen';
-// import { useEncontros } from './hook/useEncontro';
-// import { Encontros } from './data/Encontros';
-// import { useEffect, useState } from 'react';
-// import { Toaster } from 'sonner';
-// import { CategoriaCard } from './components/CategoriaCard';
-// import { Fases } from './components/Fases/Fases';
-
-// const iconePorCategoria: Record<string, React.ReactNode> = {
-//   Romântico: <span>💞</span>,
-//   Picantes: <span>🔥</span>,
-//   Gastronómicos: <span>🍲</span>,
-//   Aleatórios: <span>🎲</span>,
-//   'Ar Livre': <span>🌟</span>,
-//   Criativos: <span>🎨</span>,
-//   Culturais: <span>🎭</span>,
-//   Relaxantes: <span>🧘‍♂️</span>,
-//   Esportivos: <span>🏃‍♂️</span>,
-//   Surpresas: <span>🎁</span>,
-// };
-
-// function App() {
-//   const [faseSelecionada, setFaseSelecionada] = useState<number | null>(null);
-//   const [jogoConcluido, setJogoConcluido] = useState(false);
-
-//   const {
-//     categoriaSelecionada,
-//     setCategoriaSelecionada,
-//     cardRevelados,
-//     cardRealizados,
-//     revelarCard,
-//     toggleRealizado,
-//     getQtdRealizados,
-//     categorias,
-//     encontrosCard,
-//     modalInfo,
-//     setModalInfo,
-//     todasAsFases,
-//     setEncontrosCard,
-//     setCardRealizados,
-//     setCardRevelados,
-//     toggleAmei,
-//     cardsAmados,
-//     setFaseAtual,
-//     faseAtual,
-//     setNavegandoFaseConcluida,
-//     navegandoFaseConcluida
-//   } = useEncontros(Encontros, faseSelecionada);
-
-//   // ✅ Carregar fase e progresso
-//   useEffect(() => {
-//     if (faseSelecionada === null && faseAtual === null) return;
-
-//     const fase = faseSelecionada ?? faseAtual;
-
-//     setEncontrosCard(todasAsFases[fase]);
-
-//     const cardsReveladosPorFase = JSON.parse(localStorage.getItem("cardsReveladosPorFase") || "{}");
-//     setCardRevelados(cardsReveladosPorFase[`fase${fase}`] || {});
-
-//     const cardsRealizadosPorFase = JSON.parse(localStorage.getItem("cardsRealizadosPorFase") || "{}");
-//     setCardRealizados(cardsRealizadosPorFase[`fase${fase}`] || {});
-
-//     // ✅ Fecha modal se voltar para fase/categoria concluída
-//     const concluida = localStorage.getItem(`fase-concluida-${fase}`);
-//     if (concluida === "true" && modalInfo?.tipo === "fase" && navegandoFaseConcluida) {
-//       console.log("🔒 Fechando modal de fase concluída");
-//       setModalInfo(null);
-//       setNavegandoFaseConcluida(false);
-//     }
-//     if (concluida === "true" && modalInfo?.tipo === "categoria" && navegandoFaseConcluida) {
-//       console.log("🔒 Fechando modal de categoria concluída");
-//       setModalInfo(null);
-//       setNavegandoFaseConcluida(false);
-//     }
-//   }, [faseSelecionada, faseAtual, modalInfo?.tipo, navegandoFaseConcluida]);
-
-//   // ✅ Avançar fase
-//   const handleMudancaFase = () => {
-   
-
-
-
-//     if (modalInfo?.tipo === 'fase') {
-//       const novaFase = faseAtual + 1;
-
-//       if (!todasAsFases[novaFase]) {
-//         localStorage.setItem('jogoConcluido', 'true');
-//         setJogoConcluido(true);
-//         return;
-//       }
-
-//       localStorage.setItem('faseAtual', novaFase.toString());
-//       setFaseAtual(novaFase);
-//       setFaseSelecionada(novaFase);
-//       setCardRealizados({});
-//       setCardRevelados({});
-//       setCategoriaSelecionada(null);
-
-//     }
-//     if (modalInfo?.tipo === 'categoria') {
-//       setCategoriaSelecionada(null);
-//     }
-//     setModalInfo(null);
-//   };
-
-//   if (jogoConcluido) {
-//     return <FinalScreen />;
-//   }
-
-//   return (
-//     <>
-//       <Toaster richColors />
-//       <div className="p-6 max-w-6xl mx-auto">
-//         {faseSelecionada === null ? (
-//           <>
-//             <HeaderEncontro title="💘 Encontros Secretos" />
-//             <p className="text-center text-neutral-700 mb-10 text-lg max-w-xl mx-auto">
-//               Escolha uma fase para começar seus encontros!
-//             </p>
-//             <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
-//               <Fases
-//                 faseSelecionada={faseSelecionada}
-//                 setFaseSelecionada={setFaseSelecionada}
-//                 setNavegandoFaseConcluida={setNavegandoFaseConcluida}
-//               />
-//             </div>
-//           </>
-//         ) : !categoriaSelecionada ? (
-//           <>
-//             <div className="relative flex items-center mb-8 justify-center">
-//               <div className="absolute left-0">
-//                 <ButtonVoltar
-//                   onClick={() => {
-//                     setFaseSelecionada(null); // Voltar para fases
-//                     setCategoriaSelecionada(null);
-//                     setModalInfo(null);
-//                     setNavegandoFaseConcluida(false);
-//                   }}
-//                   name="Voltar às Fases"
-//                 />
-//               </div>
-//               <HeaderEncontro title={`📂 Fase ${faseSelecionada + 1} - Categorias`} />
-//             </div>
-
-//             <p className="text-center text-neutral-700 mb-10 text-lg max-w-xl mx-auto">
-//               Escolha uma categoria para explorar.
-//             </p>
-
-//             <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
-//               {categorias.map((categoria) => (
-//                 <CategoriaCard
-//                   key={categoria}
-//                   tipo="categoria"
-//                   nome={categoria}
-//                   total={encontrosCard[categoria]?.length || 0}
-//                   feitos={getQtdRealizados(categoria)}
-//                   onClick={() => setCategoriaSelecionada(categoria)}
-//                   icone={iconePorCategoria[categoria] || <span>❓</span>}
-//                   isCompleto={getQtdRealizados(categoria) === encontrosCard[categoria]?.length}
-//                 />
-//               ))}
-//             </div>
-//           </>
-//         ) : (
-//           <>
-//             <div className="relative flex items-center mb-8 justify-center">
-//               <div className="absolute left-0">
-//                 <ButtonVoltar
-//                   onClick={() => setCategoriaSelecionada(null)}
-//                   name="Voltar às Categorias"
-//                 />
-//               </div>
-
-//               <HeaderEncontro
-//                 title={
-//                   <>
-//                     {iconePorCategoria[categoriaSelecionada] || '❓'} {categoriaSelecionada}
-//                   </>
-//                 }
-//               />
-//             </div>
-
-//             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
-//               {encontrosCard[categoriaSelecionada]?.map((encontro) => {
-//                 const key = `${categoriaSelecionada}-${encontro.id}`;
-//                 return (
-//                   <EncontroCard
-//                     key={key}
-//                     categoria={categoriaSelecionada}
-//                     encontro={encontro}
-//                     revelado={cardRevelados[key]}
-//                     realizado={cardRealizados[key]}
-//                     onRevelar={() => revelarCard(categoriaSelecionada, encontro.id)}
-//                     onToggleRealizado={() => toggleRealizado(categoriaSelecionada, encontro.id)}
-//                     toggleAmei={() => toggleAmei(categoriaSelecionada, encontro.id)}
-//                     cardsAmados={cardsAmados}
-//                   />
-//                 );
-//               })}
-//             </div>
-//           </>
-//         )}
-
-//         {modalInfo && (
-//           <ModalParabens
-//             titulo={modalInfo.titulo}
-//             mensagem={modalInfo.mensagem}
-//             onClose={handleMudancaFase}
-//           />
-//         )}
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
 
 
 
--- src/components/EncontroCard/index.tsx
+++ src/components/EncontroCard/index.tsx
@@ -1,147 +1,3 @@
-// import { motion } from "framer-motion";
-// import { Button } from "../ui/button";
-// import { Card, CardContent } from "../ui/card";
-// import { HeartIcon } from '@heroicons/react/24/solid'
-
-// interface EncontroCardProps {
-//   encontro: {
-//     id: number;
-//     nome: string;
-//     icone: string;
-//   };
-//   categoria: string;
-
-//   revelado: boolean;
-//   realizado: boolean;
-//   onRevelar: () => void;
-//   onToggleRealizado: () => void;
-//   toggleAmei: () => void;
-//   cardsAmados:  Record<string, boolean>;
-
-// }
-
-// export const EncontroCard = ({
-//   encontro,
-//   revelado,
-//   realizado,
-//   onRevelar,
-//   onToggleRealizado,
-//   toggleAmei,
-//   cardsAmados,
-//   categoria
-  
-// }: EncontroCardProps) => {
-//   console.log(cardsAmados)
-//   return (
-//     <motion.div
-//       className="relative perspective cursor-pointer"
-//       onClick={onRevelar}
-//     >
-//       <div className="relative w-full h-46" style={{ transformStyle: 'preserve-3d' }}>
-//         <motion.div
-//           initial={false}
-//           animate={{ rotateY: revelado ? 180 : 0 }}
-//           transition={{ duration: 0.6 }}
-//           className="absolute w-full h-full"
-//           style={{ transformStyle: 'preserve-3d' }}
-//         >
-// <Card
-//       className="cursor-pointer transition bg-white absolute hover:shadow-lg border-none w-full h-full bg-pink-200 text-4xl font-bold flex items-center justify-center rounded-xl border p-2"
-//       style={{ backfaceVisibility: 'hidden' }}
-      
-     
-//     >
-//       {/* <CardContent className="p-2 text-center flex flex-col items-center justify-center gap-2 ">
-//        ?
-//       </CardContent> */}
-//       <CardContent className="p-2 text-center flex flex-col items-center justify-center gap-2">
-//   <span className="text-5xl">?</span>
-// </CardContent>
-//     </Card>
-
-// <Card
-//   className={`cursor-pointer transition w-full h-full bg-white flex items-center justify-center rounded-xl border p-2 ${
-//     realizado ? 'border-pink-500 bg-pink-50 shadow-inner' : 'hover:shadow-lg'
-//   }`}
-//   style={{ transform: 'rotateY(180deg)', backfaceVisibility: 'hidden' }}
-// >
-//   <CardContent className="p-2 text-center flex flex-col items-center justify-center gap-2 relative">
-//   {/* <button
-//       onClick={(e) => {
-//         e.stopPropagation();
-//         toggleAmei();
-//       }}
-//       className="absolute top-2 left-2 text-2xl"
-//     >
-//       <span className={cardsAmados[`${categoria}-${encontro.id}`] ? "text-pink-600" : "text-gray-400"}>
-//         ❤️
-//       </span>
-//     </button> */}
-//     <button
-//   onClick={(e) => {
-//     e.stopPropagation();
-//     toggleAmei();
-//   }}
-//   className="absolute top-2 right-2"
-// >
-//   <HeartIcon
-//     className={cardsAmados[`${categoria}-${encontro.id}`] ? "text-pink-600 w-6 h-6" : "text-gray-400 w-6 h-6"}
-//   />
-// </button>
- 
-//     {revelado && (
-//       <motion.div
-//         initial={{ opacity: 0, scale: 0.9 }}
-//         animate={{ opacity: 1, scale: 1 }}
-//         transition={{ duration: 0.4 }}
-//       >
-//         <div className={`flex flex-col items-center ${realizado ? 'opacity-60 ' : ''}`}>
-//           <div className="text-4xl mb-2">{encontro.icone}</div>
-//           <div
-//             className={`text-base font-semibold min-h-[48px] text-center ${
-//               realizado ? 'line-through text-gray-500' : ''
-//             }`}
-//           >
-//             {encontro.nome}
-//           </div>
-//         </div>
-
-//         <Button
-//   className={` cursor-pointer mt-4 w-[230px] mx-auto transition-colors duration-200 ${
-//     realizado
-//       ? ''
-//       : 'bg-pink-600 text-white hover:bg-pink-700'
-//   }`}
-//   size="sm"
-//   variant={realizado ? 'link' : 'default'}
-//   onClick={(e) => {
-//     e.stopPropagation();
-//     onToggleRealizado();
-//   }}
-// >
-//   {realizado ? 'Desfazer' : 'Marcar como feito'}
-// </Button>
-//       </motion.div>
-//     )}
-
-//     {realizado && (
-//       <div className="absolute top-2 right-2 text-green-600 text-xl">
-//         ✅
-//       </div>
-//     )}
-//   </CardContent>
-// </Card>
-
-
-
-
-
-
-//         </motion.div>
-//       </div>
-//     </motion.div>
-//   );
-// };
 
 
 
@@ -161,8 +17,8 @@ interface EncontroCardProps {
   realizado: boolean;
   onRevelar: () => void;
   onToggleRealizado: () => void;
-  toggleAmei: () => void;
-  cardsAmados: Record<string, boolean>;
+  faseConcluida: boolean; 
+
 }
 
 export const EncontroCard = ({
@@ -171,12 +27,10 @@ export const EncontroCard = ({
   realizado,
   onRevelar,
   onToggleRealizado,
-
-  cardsAmados,
-  categoria
+  faseConcluida
 }: EncontroCardProps) => {
-  const cardKey = `${categoria}-${encontro.id}`;
-  const amei = cardsAmados[cardKey];
+  
+
 
 
 
@@ -246,7 +100,7 @@ export const EncontroCard = ({
                   </div>
 
                   {/* BOTÃO DE MARCAR COMO FEITO */}
-                  <Button
+                  {/* <Button
                     className={` cursor-pointer mt-4 w-full sm:w-[230px] transition-colors duration-200 ${
                       realizado ? '' : 'bg-pink-600 text-white hover:bg-pink-700'
                     }`}
@@ -258,7 +112,24 @@ export const EncontroCard = ({
                     }}
                   >
                     {realizado ? 'Desfazer' : 'Marcar como feito'}
-                  </Button>
+                  </Button> */}
+
+
+                  {!faseConcluida && (
+  <Button
+    className={` cursor-pointer mt-4 w-full sm:w-[230px] transition-colors duration-200 ${
+      realizado ? '' : 'bg-pink-600 text-white hover:bg-pink-700'
+    }`}
+    size="sm"
+    variant={realizado ? 'link' : 'default'}
+    onClick={(e) => {
+      e.stopPropagation();
+      onToggleRealizado();
+    }}
+  >
+    {realizado ? 'Desfazer' : 'Marcar como feito'}
+  </Button>
+)}
                 </motion.div>
               )}
 
